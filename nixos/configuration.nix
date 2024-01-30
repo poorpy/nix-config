f0ab77d9532896c21edf65fdcec524cbdb9e7883
bartosz marczyński <marczynski.bartosz@gmail.com>
2024-01-30 16:28:42 +0100

    fix: add extra portals

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -158,6 +158,9 @@
   };
 
   xdg.portal.enable = true;
+  xdg.portal.wlr.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-wlr ];
+
 
   services.dbus = {
     packages = [ pkgs.gcr ];

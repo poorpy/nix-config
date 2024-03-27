83ac1aaab1500012e667202a1b6ef390868da370
bartosz marczyński <marczynski.bartosz@gmail.com>
2024-03-27 21:26:05 +0100

    chore: bump go to 1.22

--- home-manager/go.nix
+++ home-manager/go.nix
@@ -1,22 +1,22 @@
 { inputs, pkgs, lib, config, ... }: {
   programs.go = {
     enable = true;
-    package = pkgs.master.go_1_21;
+    package = pkgs.master.go_1_22;
   };
 
   home.packages =
     let
       lint-langserver-override =
         pkgs.unstable.golangci-lint-langserver.override {
-          buildGoModule = pkgs.master.buildGo121Module;
+          buildGoModule = pkgs.master.buildGo122Module;
         };
       lint-override =
         pkgs.unstable.golangci-lint.override {
-          buildGoModule = pkgs.master.buildGo121Module;
+          buildGoModule = pkgs.master.buildGo122Module;
         };
       gopls-override =
         pkgs.unstable.gopls.override {
-          buildGoModule = pkgs.master.buildGo121Module;
+          buildGoModule = pkgs.master.buildGo122Module;
         };
     in
     with pkgs; [

346b347b2e663d21d38e9f2b9d4f47a842121a5a
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2025-11-24 23:08:50 +0100

    feat: add lua5.1 instead of luajit, add luarocks

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    luajitPackages.luarocks
+    lua51Packages.lua
+    lua51Packages.luarocks
     sumneko-lua-language-server
     vscode-langservers-extracted
     stylua

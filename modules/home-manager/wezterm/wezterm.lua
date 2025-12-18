34659c82088dfbba9ee2a226a84ff85fb8f0cdcc
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2025-12-18 22:27:43 +0100

    feat: adjust wezterm config

--- modules/home-manager/wezterm/wezterm.lua
+++ modules/home-manager/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 
 local function is_darwin()
 	local handle = io.popen("uname")
@@ -10,13 +11,21 @@ local function is_darwin()
 	return result == "Darwin\n"
 end
 
+local function is_in_path(cmd)
+	local query = string.format("command -v %s > /dev/null 2>&1", cmd)
+	local result = os.execute(query)
+	return result == 0
+end
+
 local font_size = is_darwin() and 14 or 11
 local fish = os.getenv("HOME") .. "/.nix-profile/bin/fish"
+local zsh = os.getenv("HOME") .. "/.nix-profile/bin/zsh"
+local default_prog = is_in_path("fish") and { fish, "-l" } or { zsh }
 
 local config = {
 	font_size = font_size,
 	font = wezterm.font("JetBrains Mono"),
-	default_prog = { fish, "-l" },
+	default_prog = default_prog,
 	use_fancy_tab_bar = false,
 	enable_scroll_bar = false,
 	enable_tab_bar = true,
@@ -26,73 +35,22 @@ local config = {
 		top = 0,
 		bottom = 0,
 	},
-	leader = { key = "s", mods = "CTRL" },
 	mux_enable_ssh_agent = false,
 	disable_default_key_bindings = true,
 	hide_mouse_cursor_when_typing = false,
 	enable_wayland = false,
 	color_scheme = "nordfox",
 	keys = {
-		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
-		{
-			key = "n",
-			mods = "LEADER",
-			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
-		},
-		{
-			key = "v",
-			mods = "LEADER",
-			action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-		},
-		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-		{ key = "d", mods = "LEADER|SHIFT", action = wezterm.action.ShowDebugOverlay },
-		{
-			key = "t",
-			mods = "LEADER",
-			action = wezterm.action.SpawnCommandInNewTab({ args = { "zsh" }, cwd = "~" }),
-		},
-		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-		{ key = "J", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
-		{ key = "K", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
-		{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
-		{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
-		{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
-		{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
-		{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
-		{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
-		{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
-		{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
-		{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
-		{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-		{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-		{
-			key = "f",
-			mods = "LEADER",
-			action = wezterm.action_callback(function(win, pane)
-				local tab = win:active_tab()
-				local below = tab:get_pane_direction("Down")
-				local above = tab:get_pane_direction("Up")
-				local cwd = pane:get_foreground_process_info().cwd or "~"
-				if below == nil and above == nil then
-					pane:split({ direction = "Bottom", size = 0.15, cwd = cwd })
-					return
-				end
-				if below == nil and above ~= nil then
-					above:activate()
-					tab:set_zoomed(true)
-					return
-				end
-				if below ~= nil and above == nil then
-					tab:set_zoomed(false)
-					below:activate()
-					return
-				end
-			end),
-		},
+		{ key = "D", mods = "CTRL|SHIFT", action = action.ShowDebugOverlay },
+		{ key = "t", mods = "ALT", action = action.SpawnTab("CurrentPaneDomain") },
+		{ key = "w", mods = "ALT", action = action.CloseCurrentTab({ confirm = true }) },
+		{ key = "n", mods = "ALT", action = action.ActivateTabRelative(1) },
+		{ key = "p", mods = "ALT", action = action.ActivateTabRelative(-1) },
+		{ key = "1", mods = "ALT", action = action.ActivateTab(0) },
+		{ key = "2", mods = "ALT", action = action.ActivateTab(1) },
+		{ key = "3", mods = "ALT", action = action.ActivateTab(2) },
+		{ key = "4", mods = "ALT", action = action.ActivateTab(3) },
+		{ key = "5", mods = "ALT", action = action.ActivateTab(4) },
 
 		{ key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
 		{ key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },

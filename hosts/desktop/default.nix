3f532a9257d22bf181ec3ec7250ccca01624c915
Bartosz Marczyński <marczynski.bartosz@gmail.com>
2026-05-31 14:04:56 +0200

    fix: add usb-modeswitch to fix wifi adapter

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -19,6 +19,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
+  systemd.services.rtl8852cu-modeswitch = {
+    description = "Switch RTL8852CU adapter from CD-ROM to WLAN mode";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.usb-modeswitch}/bin/usb_modeswitch -K -v 0bda -p 1a2b";
+    };
+  };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="1a2b", TAG+="systemd", ENV{SYSTEMD_WANTS}+="rtl8852cu-modeswitch.service"
+  '';
 
   services.blueman.enable = true;
 
